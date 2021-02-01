362535772c07f8f28402a0301a2818ba5c7fd6ec
Nodal2 <archireckon@gmail.com>
2021-02-01 14:51:20 +0100

    renommer package

---
+++ src/application/Main.java
@@ -0,0 +1,17 @@
+package application;
+import javax.swing.JFrame;
+
+import vue.Affichage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Affichage affichage = new Affichage();
+		JFrame fenetre = new JFrame("Projet"); //instancie une fenetre avec un titre
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //permet de quitter le programme quand on clique sur la croix
+		fenetre.add(affichage); //ajoute le component a la fenetre
+		fenetre.pack();
+		fenetre.setVisible(true); //rend la fenetre visible
+	}
+
+}
