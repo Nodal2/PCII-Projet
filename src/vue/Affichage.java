99060da0e90f11d75ff1b1712a229d9057730abd
diika91 <78360111+diika91@users.noreply.github.com>
2021-02-08 15:28:41 +0100

    Dessin des décorations sur le terrain

--- src/vue/Affichage.java
+++ src/vue/Affichage.java
@@ -28,12 +28,21 @@ public class Affichage extends JPanel {
 	public void paint(Graphics g) {
 		super.paint(g); //permet de nettoyer l'image
 		g.drawOval(this.voiture.getPosX(), this.voiture.getPosY(), Voiture.LARGEUR_VOITURE , Voiture.HAUTEUR_VOITURE); //Permet de dessiner un ovale
-		g.drawLine(decor.posxlignep1, decor.posyligne, decor.posxlignep2, decor.posyligne);
+		g.drawLine(decor.posxlignep1, decor.posyligne, decor.posxlignep2, decor.posyligne); // dessine la ligne d'horizon
+
+		/** dessin de la ligne brisée pour les montagnes */
 		for(int i = 0; i<this.decor.getMontagne().size()-1; i++){
 			Point p1 = this.decor.getMontagne().get(i);
 			Point p2 = this.decor.getMontagne().get(i+1);
 
 			g.drawLine(p1.x, p1.y, p2.x, p2.y);
+		}
+
+		/** dessin des décors autours de la route */
+		for(int i = 0; i<this.decor.getDecoration().size()-1; i++){
+			Point p1 = this.decor.getDecoration().get(i);
+
+			g.drawRect(p1.x, p1.y, decor.DECOR_LARG, decor.DECOR_HAUT);
 		}
 
 	}
